def1caa62adaa5438747b07686e35997cb7310d5
JAVA <moh.java4@yahoo.com>
2020-08-04 23:24:42 +0430

    adding some characteristics of a person to index file

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
-echo 'Hello world!' . '<br/>' . PHP_EOL;
 
 // printing some characteristics of a person
+
+$student = [
+	'id' => 1,
+	'name' => 'Mohsen',
+	'family' => 'Hosseini',
+	'father' => 'Ali',
+	'grade' => 17.25,
+	'passed' => true,
+];
+
+$keys = array_keys($student);
+$c = count($keys);
+
+for($i = 0; $i < $c; $i++){
+	echo '<p>' . $keys[$i] . " : " . $student[$keys[$i]] . '</p>';
+}
+
 ?>
